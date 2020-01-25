f4069f4eb5f2723a6f4e321691fd4c6424986133
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-25 08:56:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,8 @@ name_hash = {
   "plants" => 17
   }
 def key_for_min_value(name_hash)
-  if name_hash
+  if name_hash = {}
+    nil
     name_hash.sort do |key, value|
     key <=> value
   end.first[0]
