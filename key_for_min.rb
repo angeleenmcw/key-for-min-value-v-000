d71f8d8e88f51f910cd8cce390c9fe16e9908967
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-25 08:32:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,11 @@
 
 def key_for_min_value(name_hash)
 name_hash = {
-  "shape" => "square",
-  drink => "margarita",
+  "shape" => 20,
+  "drinks" => 6,
+  "plants" => 17
+  }
   
-}
+key_for_min_value.each do |minimum|
+  
 end
